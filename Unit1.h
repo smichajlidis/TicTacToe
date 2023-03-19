77c889ec568191a2e8cf28f6ae8c6cd967f928dc
smichajlidis <smichajlidis@gmail.com>
2023-03-19 20:56:59 +0100

    Add interface elements

--- Unit1.h
+++ Unit1.h
@@ -7,10 +7,23 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
+#include <ExtCtrls.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TImage *field1;
+        TImage *field2;
+        TImage *field3;
+        TImage *field4;
+        TImage *field5;
+        TImage *field6;
+        TImage *field7;
+        TImage *field8;
+        TImage *field9;
+        TLabel *Label1;
+        TImage *Image1;
+        TButton *Button1;
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
